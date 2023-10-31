f86694d2bec7952f363f66d89a9ee2ee771a09ce
Alec L. Robitaille <robit.alec@gmail.com>
2023-10-31 10:30:30 -0300

    fst _targets.R

---
+++ _targets.R
@@ -0,0 +1,21 @@
+# === Targets: Memory Movement --------------------------------------------
+# Alec L. Robitaille
+# Based on https://github.com/kimx3725/Memory_Movement
+# Kim, D., Thompson, P., Wolfson, D., Merkle, J., Oliveira-Santos, L.G.R.,
+#  Forester, J., Avgar, T., Lewis, M. and Fieberg, J., 2023.
+#  Identifying signals of memory from observations of animal movements
+#  in Plato's cave. bioRxiv, pp.2023-08.
+
+
+
+# Source ------------------------------------------------------------------
+targets::tar_source('R')
+
+
+
+
+# Variables ---------------------------------------------------------------
+
+
+
+
